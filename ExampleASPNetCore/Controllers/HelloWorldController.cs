5033f2a573427df49ee2071311746001087fc0b6
damiusen <martin_ar_91@hotmail.com>
2017-12-20 22:59:31 -0300

    First Upload of the Project. - Images used for the Step by Step Tutorial. - Example Project (ASP.Net MVC Core).

---
+++ ExampleASPNetCore/Controllers/HelloWorldController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+
+namespace ExampleASPNetCore.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ ExampleASPNetCore/Data/ExampleASPNetCoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExampleASPNetCore.Models
+{
+    public class ExampleASPNetCoreContext : DbContext
+    {
+        public ExampleASPNetCoreContext (DbContextOptions<ExampleASPNetCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExampleASPNetCore.Models.Movie> Movie { get; set; }
+    }
+}
